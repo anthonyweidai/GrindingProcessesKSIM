3f9a80f75192b25eaa7798adc57219f8f753ffdd
beenoera <beenoera@gmail.com>
2020-12-25 08:51:47 +0800

    Dont merge this one

--- batch_efodist.m
+++ batch_efodist.m
@@ -3,7 +3,7 @@ create_folder();
 %% if the programme works well, then use batnum 2
 geoparam.shape = 1;
 for batnum = 1:10
-    for wheel_type = 3
+    for wheel_type = 2
         sepparam.wheel_type = wheel_type;
         geoparam.fillet_mode = 1;
         geoparam.RA_mode = 0;
@@ -12,13 +12,16 @@ for batnum = 1:10
             geoparam.omega = omega;
             for Rarea = [0.3, 0.7]
                 geoparam.Rarea = Rarea;
-                FOI = 'EFdist';  % field of interest
+                FOI = 'EFdist_new';  % field of interest
                 if wheel_type == 2 % within different distribution
                     for theta = [15, 30, 45, 60, 75]
                         sepparam.theta = theta;
                         sepparam.RowGap = 100;
                         sepparam.SaveGap = 25;
                         sepparam.LS_mode = 1;
+                        sepparam.k_dev = 0;
+                        geoparam.Rsigma = 2; %R=10, +3sigma=6,sigma=2; trimming height=10
+                        geoparam.trim_h = 10; %50%trim, trim back to the average height
                         GrdProcess4(batnum,FOI,sepparam,geoparam);
                     end
                 elseif wheel_type == 3
