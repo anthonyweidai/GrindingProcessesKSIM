d039116e96845b9d73265d4819a17bf909ceb893
Davidietop <davidietop@163.com>
2021-01-23 11:41:06 +0800

    rm batch

--- batch_UT_V3.m
+++ batch_UT_V3.m
@@ -2,10 +2,10 @@ set(0,'defaultfigurecolor',[1 1 1])
 create_folder();
 %%
 batnum = 1;
-parallel_mode = 1;
+parallel_mode = 0;
 % FOI_edges(batnum, parallel_mode);
 FOI_ConeAngle(batnum, parallel_mode);
-FOI_sigmasw(batnum, parallel_mode);
+% FOI_sigmasw(batnum, parallel_mode);
 
 % FOI_ELSRarea(batnum, parallel_mode);
 
