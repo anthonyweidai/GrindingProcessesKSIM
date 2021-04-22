af84197761c96874343c91651b72971b09f156de
Anthony Dave <davidietop@163.com>
2021-04-22 10:35:12 +0800

    'update'

---
+++ grindingProcess.m
@@ -0,0 +1,59 @@
+%%%% list of functions
+%   GrdProcess2
+%-------------------------------step
+%1  - TGW_generator
+%   -- wheel_generator
+%   === get_octacube.m
+%1  - bubbleSimulator_cluster
+%   -- updatePosition
+%   -- wheel_generator
+%   === get_octacube.m
+%-------------------------------step
+%2  - GrindingProcess   % simulation process
+%   -- COF_CAL
+%%
+function grindingProcess(cof_cal_mode, Cycle, FOI, SepParam, GeoParam)
+warning off;
+tic;
+%%
+DiskPath = 'M:/GrdData/';
+%%
+WorkpieceLength = 1000;
+WorkpieceWidth = WorkpieceLength/2;     % max(grits.posx);
+WheelLength = 15*WorkpieceLength;       % 40 times Wheel Diameter to Width
+WheelWidth = WorkpieceWidth;
+vw = 200e3; % feed speed, um/s
+res = .2;  % surf resolution of all axis
+%% Input parameters initialization
+[SepParam, GeoParam] = initializeParam(SepParam, GeoParam);
+%% Judge of exsistence
+[Existence, ~] = batchExsistInvalidate(DiskPath, Cycle, FOI, SepParam, GeoParam, 1);
+if Existence == 1
+    disp('Aleady run with given parameters, skill to the next')
+else
+%% Simplified mode won't calculate force, but the other will
+% cof_cal_mode(GeoParam.Shape==2 || GeoParam.Shape==3) = 0; % 0-simplified mode 1-accurate mode
+%% Generate grinding wheel
+FileName = getFilename(DiskPath, Cycle, SepParam, GeoParam, FOI, vw);
+if SepParam.WheelType == 1
+    [grits,GritsProfile,GeoParam] = bubbleSimulator(FileName,WheelLength,WheelWidth,GeoParam,res);
+elseif SepParam.WheelType == 2
+    [grits,GritsProfile,GeoParam] = montecarloGenerator(FileName,WheelLength,WheelWidth,SepParam,GeoParam,res);
+elseif SepParam.WheelType == 3
+    [grits,GritsProfile,GeoParam] = TGWGenerator(FileName,WheelLength,WheelWidth,SepParam,GeoParam,res);
+end
+%% Kinematic simulation
+GrdOutput = kinematicSimulation(FileName,grits,GritsProfile,cof_cal_mode,...
+    WorkpieceLength,WorkpieceWidth,WheelLength,...
+    GeoParam,res,vw);
+%% Record input and output variables
+BatchInfo = array2table(convertCharsToStrings(char(datetime)),'VariableNames',{'datetime'});
+BatchInfo = [BatchInfo table(Cycle) table(vw)];
+BatchInfo = [BatchInfo struct2table(SepParam) struct2table(GeoParam)];
+BatchInfo = [BatchInfo GrdOutput];
+writetable(BatchInfo,...
+    [DiskPath FOI '/' 'CY' num2str(Cycle) 'wheel' num2str(SepParam.WheelType) '-info.csv'],...
+    'WriteMode','append');
+toc;
+end
+end
